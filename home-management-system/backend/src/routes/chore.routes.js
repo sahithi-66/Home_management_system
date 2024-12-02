b5abd34430918cd2a48379e3050c52a7de5f091e
syanamala-umass <syanamala@umass.edu>
2024-12-02 17:13:07 -0500

    chore.routes.js

--- home-management-system/backend/src/routes/chore.routes.js
+++ home-management-system/backend/src/routes/chore.routes.js
@@ -1,6 +1,17 @@
 import express from 'express';
-
+import ChoreController from '../controllers/ChoreController.js';
 
 const router = express.Router();
+
+
+// Regular Chore board routes
+ router.post('/addChore', ChoreController.addChore);
+ router.get('/fetchAll', ChoreController.getAllChores);
+ router.get('/schedule/:id', ChoreController.getChoreSchedule);
+ router.delete('/deleteChore/:id', ChoreController.deleteChore);
+ router.post('/swapSchedules', ChoreController.swapChoreSchedule);
+// router.put('/:id', ChoreController.updateChore);
+// router.delete('/:id', ChoreController.deleteChore);
+
 
 export default router;
