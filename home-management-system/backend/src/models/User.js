a107d6c3a9034f39da846bebd46909dd260f82b3
syanamala-umass <syanamala@umass.edu>
2024-12-02 17:21:06 -0500

    Update User.js

--- home-management-system/backend/src/models/User.js
+++ home-management-system/backend/src/models/User.js
@@ -11,6 +11,16 @@ class User {
         const [result] = await db.execute('INSERT INTO users (username, password) VALUES (?, ?)', [username, hashedPassword]);
         return result.insertId;
     }
+
+    static async findAllUsers() {
+        try {
+            const [rows] = await db.execute('SELECT * FROM users');
+            return rows; // Return the rows fetched from the database
+        } catch (error) {
+            console.error('Error fetching users:', error);
+            throw new Error('Database query failed'); // Rethrow or handle the error appropriately
+        }
+      }
 }
 
 export default User;
