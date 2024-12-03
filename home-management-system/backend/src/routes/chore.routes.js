cbcfb008095c4a7e956f4fe938d2d8c218c80376
syanamala-umass <syanamala@umass.edu>
2024-12-03 11:29:24 -0500

    Update chore.routes.js

--- home-management-system/backend/src/routes/chore.routes.js
+++ home-management-system/backend/src/routes/chore.routes.js
@@ -3,20 +3,15 @@ import ChoreController from '../controllers/ChoreController.js';
 
 const router = express.Router();
 
-// Create routes
-router.post('/', ChoreController.createChore);
-router.post('/rotation', ChoreController.createChoreWithSchedule);
 
-// Read routes
-router.get('/', ChoreController.getAllChores);
-router.get('/:id', ChoreController.getChoreById);
-router.get('/:id/schedule', ChoreController.getChoreSchedule);
+// Regular Chore board routes
+ router.post('/addChore', ChoreController.addChore);
+ router.get('/fetchAll', ChoreController.getAllChores);
+ router.get('/schedule/:id', ChoreController.getChoreSchedule);
+ router.delete('/deleteChore/:id', ChoreController.deleteChore);
+ router.post('/swapSchedules', ChoreController.swapChoreSchedule);
+// router.put('/:id', ChoreController.updateChore);
+// router.delete('/:id', ChoreController.deleteChore);
 
-// Update routes
-router.put('/:id', ChoreController.updateChore);
-router.post('/:id/rotate', ChoreController.rotateChoreAssignment);
-
-// Delete route
-router.delete('/:id', ChoreController.deleteChore);
 
 export default router;
